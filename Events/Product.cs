677c565b54c01ff02593d46d0361fbe769d7338b
Samuel Jason <Samuelljason@gmail.com>
2024-03-03 21:19:33 -0300

    Eventos

---
+++ Events/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void PriceChanged(decimal OldPrice, decimal NewPrice);
+
+    public class Product
+    {
+        private decimal _price;
+
+        public event PriceChanged PriceChanged;
+
+        public decimal price
+        {
+            get => _price;
+            set { 
+               if (value != _price)
+                {
+                    var oldPrice = _price;
+                    _price = value;
+                    PriceChanged?.Invoke(oldPrice, _price);
+                }
+            }
+        }
+        
+
+
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+        var room = new Room(3);
+        room.RoomSoldOutEvent += OnRoomSoldOut;
+    }
+
+
+    static void OnRoomSoldOut(object sender, EventArgs e)
+    {
+        Console.WriteLine("Sala lotada.");
+    }
+
+}
+
+public class Room
+{
+    public Room(int seats) 
+    { 
+        Seats = seats;
+        seatsInUse = 0;
+    }
+
+    private int seatsInUse = 0;
+    public int Seats { get; set; }
+
+    public void ReserveSeat()
+    {
+        seatsInUse++;
+
+        if (seatsInUse >= Seats)
+        {
+            OnRoomSoldOut(EventArgs.Empty); 
+        }
+        else
+        {
+            Console.WriteLine("Assento reservado");
+        }
+    }
+    
+    public event EventHandler RoomSoldOutEvent;
+
+    protected virtual void OnRoomSoldOut(EventArgs e)
+    {
+        EventHandler handler = RoomSoldOutEvent;
+        handler?.Invoke(null, e);
+    }
+}
